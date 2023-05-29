e5472cb60c0cdc3c4490bbfd1dd4f0492ea3df5c
Michael Duren <michaeld@michaelduren.com>
2023-05-29 08:56:17 -0500

    created sql create table and date inserts

---
+++ create.sql
@@ -0,0 +1,17 @@
+CREATE TABLE todos (
+	"id" serial primary key,
+	"name" VARCHAR(60),
+	"image" VARCHAR(60),
+	"dateCreated" DATE,
+	"dueDate" DATE,
+	"description" TEXT,
+	"isCompleted" boolean,
+	"priority" smallint
+)
+
+INSERT INTO todos (name, image, "dateCreated", "dueDate", description, "isCompleted", "priority")
+VALUES ('create database', 'https://cdn.guru99.com/images/1/092818_0513_PostgreSQLC9.png', NOW(), '2023-05-29', 'Create a database and dummy data for development', false, 2)
+
+
+INSERT INTO todos (name, image, "dateCreated", "dueDate", description, "isCompleted", "priority")
+VALUES ('create pg routes', 'https://cdn.guru99.com/images/1/092818_0513_PostgreSQLC9.png', NOW(), '2023-05-29', 'Create pg get, put, push, del routes etc.', false, 2)
