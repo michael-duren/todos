0acf0447c0d099041b27642282174a5ca4579dec
Michael Duren <michaeld@michaelduren.com>
2023-06-07 12:06:31 -0500

    configured vite proxy

--- src/components/MainPanel.tsx
+++ src/components/MainPanel.tsx
@@ -1,8 +1,19 @@
+import { useContext } from 'react';
+import { GeneralContext, IGeneralContext } from '../context/GeneralContext';
+
 export default function MainPanel() {
+  const { todoList } = useContext(GeneralContext) as IGeneralContext;
+  console.log(todoList);
+
   return (
     <div className="h-full w-full">
       <h2>Welcome Michael Duren</h2>
       <h3 className="text-2xl">TODOs:</h3>
+      <ul>
+        {todoList.map((todo) => {
+          return <li key={todo.name}>{todo.name}</li>;
+        })}
+      </ul>
     </div>
   );
 }
--- src/context/GeneralContext.tsx
+++ src/context/GeneralContext.tsx
@@ -1,8 +1,11 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
+import ToDo from '../models/todo';
+import axios, { AxiosResponse } from 'axios';
 
 export interface IGeneralContext {
   isModalOpen: boolean;
   setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  todoList: ToDo[];
 }
 
 export const GeneralContext = createContext<IGeneralContext | null>(null);
@@ -13,8 +16,18 @@ interface Props {
 
 export const GeneralContextProvider = ({ children }: Props) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [todoList, setTodoList] = useState<ToDo[]>([]);
+
+  useEffect(() => {
+    axios
+      .get('/api/todo')
+      .then((res: AxiosResponse) => res.data)
+      .then((todos) => setTodoList(todos))
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
-    <GeneralContext.Provider value={{ isModalOpen, setIsModalOpen }}>
+    <GeneralContext.Provider value={{ isModalOpen, setIsModalOpen, todoList }}>
       {children}
     </GeneralContext.Provider>
   );
