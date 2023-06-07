51a6a7a176bd230f4e6fc1a9a1ccd3d2f61e1662
Michael Duren <michaeld@michaelduren.com>
2023-06-07 09:35:25 -0500

    update sql script

--- create.sql
+++ create.sql
@@ -6,13 +6,11 @@ CREATE TABLE todos (
 	"dueDate" DATE,
 	"description" TEXT,
 	"isCompleted" boolean,
-	"priority" smallint,
+	"priority" VARCHAR(10),
 	"category" VARCHAR(10)
 )
 
 INSERT INTO todos (name, image, "dateCreated", "dueDate", description, "isCompleted", "priority", "category")
-VALUES ('create database', 'https://cdn.guru99.com/images/1/092818_0513_PostgreSQLC9.png', NOW(), '2023-05-29', 'Create a database and dummy data for development', false, 2, "work")
-
-
-INSERT INTO todos (name, image, "dateCreated", "dueDate", description, "isCompleted", "priority")
-VALUES ('create pg routes', 'https://cdn.guru99.com/images/1/092818_0513_PostgreSQLC9.png', NOW(), '2023-05-29', 'Create pg get, put, push, del routes etc.', false, 2, "school")
+VALUES 
+('create database', 'https://cdn.guru99.com/images/1/092818_0513_PostgreSQLC9.png', NOW(), '2023-05-29', 'Create a database and dummy data for development', false, 'High', 'work'),
+('create pg routes', 'https://cdn.guru99.com/images/1/092818_0513_PostgreSQLC9.png', NOW(), '2023-06-28', 'Create pg get, put, push, del routes etc.', false, 'Medium', 'school');
