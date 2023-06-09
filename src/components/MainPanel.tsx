ecc455bd0a994651f078a340238830e2fc87af01
Michael Duren <michaeld@michaelduren.com>
2023-06-09 08:37:39 -0500

    refactored sorting in mainpanel and made util file for sorting options

--- src/components/MainPanel.tsx
+++ src/components/MainPanel.tsx
@@ -2,44 +2,15 @@ import { useContext, useState } from 'react';
 import { GeneralContext, IGeneralContext } from '../context/GeneralContext';
 import SmallItemCard from './SmallItemCard';
 import Listbox from './Listbox';
+import { sortBy } from '../utils/sort';
 
 export default function MainPanel() {
-  const { unCompleteToDoList: todoList } = useContext(
-    GeneralContext
-  ) as IGeneralContext;
+  const { unCompleteToDoList } = useContext(GeneralContext) as IGeneralContext;
   const [orderBy, setOrderBy] = useState<'Due Date' | 'Priority' | 'Category'>(
     'Due Date'
   );
   const orderOptions = ['Due Date', 'Priority', 'Category'];
-
-  const orderedByDate = [...todoList].sort((a, b) => {
-    const dateA = new Date(a.dateDue);
-    const dateB = new Date(b.dateDue);
-
-    return dateA.getTime() - dateB.getTime();
-  });
-
-  const orderByPriority = [...todoList].sort((a, b) => {
-    const priorityA =
-      a.priority.toLowerCase() === 'high'
-        ? 1
-        : a.priority.toLowerCase() === 'medium'
-        ? 2
-        : 3;
-    const priorityB =
-      b.priority.toLowerCase() === 'high'
-        ? 1
-        : b.priority.toLowerCase() === 'medium'
-        ? 2
-        : 3;
-
-    return priorityA < priorityB ? -1 : priorityA > priorityB ? 1 : 0;
-  });
-
-  const orderByCategory = [...todoList].sort((a, b) => {
-    return a.category.localeCompare(b.category);
-  });
-
+  const sortedTodos = sortBy(orderBy, unCompleteToDoList);
   return (
     <div className="col-span-6 flex justify-center">
       <div className="h-full w-full">
@@ -54,29 +25,13 @@ export default function MainPanel() {
           />
         </div>
         <ul>
-          {orderBy === 'Due Date'
-            ? orderedByDate.map((todo) => {
-                return (
-                  <li key={todo.id}>
-                    <SmallItemCard todo={todo} />
-                  </li>
-                );
-              })
-            : orderBy === 'Priority'
-            ? orderByPriority.map((todo) => {
-                return (
-                  <li key={todo.id}>
-                    <SmallItemCard todo={todo} />
-                  </li>
-                );
-              })
-            : orderByCategory.map((todo) => {
-                return (
-                  <li key={todo.id}>
-                    <SmallItemCard todo={todo} />
-                  </li>
-                );
-              })}
+          {sortedTodos.map((todo) => {
+            return (
+              <li key={todo.id}>
+                <SmallItemCard todo={todo} />
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
---
+++ src/utils/sort.ts
@@ -0,0 +1,47 @@
+import ToDo from '../models/todo';
+
+export const orderByDate = (todoList: ToDo[]) => {
+  return [...todoList].sort((a, b) => {
+    const dateA = new Date(a.dateDue);
+    const dateB = new Date(b.dateDue);
+
+    return dateA.getTime() - dateB.getTime();
+  });
+};
+
+export const orderByPriority = (todoList: ToDo[]) => {
+  return [...todoList].sort((a, b) => {
+    const priorityA =
+      a.priority.toLowerCase() === 'high'
+        ? 1
+        : a.priority.toLowerCase() === 'medium'
+        ? 2
+        : 3;
+    const priorityB =
+      b.priority.toLowerCase() === 'high'
+        ? 1
+        : b.priority.toLowerCase() === 'medium'
+        ? 2
+        : 3;
+
+    return priorityA < priorityB ? -1 : priorityA > priorityB ? 1 : 0;
+  });
+};
+
+export const orderByCategory = (todoList: ToDo[]) => {
+  return [...todoList].sort((a, b) => a.category.localeCompare(b.category));
+};
+
+export const sortBy = (
+  sortOption: 'Due Date' | 'Priority' | 'Category',
+  todoList: ToDo[]
+): ToDo[] => {
+  switch (sortOption) {
+    case 'Priority':
+      return orderByPriority(todoList);
+    case 'Category':
+      return orderByCategory(todoList);
+    default:
+      return orderByDate(todoList);
+  }
+};
