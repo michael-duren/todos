4d936a1a842bc6aeb8a0ad3d04c1c434c7a48bec
Michael Duren <michaeld@michaelduren.com>
2023-07-01 16:28:52 -0500

    converted back to heroku deployment version

--- server/server.ts
+++ server/server.ts
@@ -1,12 +1,12 @@
 import express from 'express';
-// import path from 'path';
+import path from 'path';
 const app = express();
 import todoRouter from './routes/todoRouter.ts';
 const PORT = Number(process.env.PORT) || 5000;
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(express.json()); // needed for axios requests
-// app.use(express.static(path.join(__dirname, '../../../client')));
+app.use(express.static(path.join(__dirname, '../../../client')));
 
 /** ---------- EXPRESS ROUTES ---------- **/
 app.use('/api/todo', todoRouter);
